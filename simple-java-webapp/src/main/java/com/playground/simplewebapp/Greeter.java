abde3dc8c225689994f5d9add9bf5f866fb256db
mattia.battiston <mattia.battiston@gmail.com>
2014-05-11 19:43:15 +0100

    created a servlet for greetings

--- simple-java-webapp/src/main/java/com/playground/simplewebapp/Greeter.java
+++ simple-java-webapp/src/main/java/com/playground/simplewebapp/Greeter.java
@@ -1,6 +1,25 @@
 package com.playground.simplewebapp;
 
-public class Greeter {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Greeter extends HttpServlet {
+
+    public static final String USERNAME_PARAMETER = "username";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Greeting user...");
+
+        String username = request.getParameter(USERNAME_PARAMETER);
+
+        String result = "Hello " + username + "!";
+
+        response.getWriter().print(result);
+    }
 
     public String greet(String name) {
         return "Hello " + name;
