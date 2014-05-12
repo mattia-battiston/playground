03bfaca484ab23ece06ed9f5307dd9739fbfc31a
mattia.battiston <mattia.battiston@gmail.com>
2014-05-12 21:06:26 +0100

    tests to make sure the servlet works as expected

--- jsp-servlet-webapp/src/main/java/com/playground/simplewebapp/Greeter.java
+++ jsp-servlet-webapp/src/main/java/com/playground/simplewebapp/Greeter.java
@@ -11,6 +11,9 @@ public class Greeter extends HttpServlet {
 
     public static final String USERNAME_PARAMETER = "username";
 
+    public static final String GREETING_MESSAGE_PARAMETER = "greetingMessage";
+    public static final String GREETING_RESULT_VIEW = "greetingResult.jsp";
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Greeting user...");
@@ -19,14 +22,10 @@ public class Greeter extends HttpServlet {
 
         String result = "Hello " + username + "!";
 
-        request.setAttribute("greetingMessage", result);
+        request.setAttribute(GREETING_MESSAGE_PARAMETER, result);
 
-        RequestDispatcher dispatcher = request.getRequestDispatcher("greetingResult.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher(GREETING_RESULT_VIEW);
         dispatcher.forward(request, response);
-    }
-
-    public String greet(String name) {
-        return "Hello " + name;
     }
 
 }
--- jsp-servlet-webapp/src/test/java/com/playground/simplewebapp/GreeterTest.java
+++ jsp-servlet-webapp/src/test/java/com/playground/simplewebapp/GreeterTest.java
@@ -2,17 +2,64 @@ package com.playground.simplewebapp;
 
 import org.junit.Test;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static com.playground.simplewebapp.Greeter.GREETING_MESSAGE_PARAMETER;
+import static com.playground.simplewebapp.Greeter.GREETING_RESULT_VIEW;
+import static com.playground.simplewebapp.Greeter.USERNAME_PARAMETER;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class GreeterTest {
 
+    Greeter greeter = new Greeter();
+
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    RequestDispatcher requestDispatcher = mock(RequestDispatcher.class);
+
     @Test
-    public void saysHello() throws Exception {
-        Greeter greeter = new Greeter();
+    public void forwardsToTheGreetingsPage() throws ServletException, IOException {
+        givenARequestIsComingInForUser("Mattia");
 
-        String result = greeter.greet("Mattia");
+        whenTheRequestIsReceived();
 
-        assertThat(result, is("Hello Mattia"));
+        thenTheRequestIsForwardedToTheGreetingsPage();
     }
+
+    @Test
+    public void greetsUserOnTheGreetingsPage() throws Exception {
+        givenARequestIsComingInForUser("Mattia");
+
+        whenTheRequestIsReceived();
+
+        thenTheGreetinsPageWillGreetTheUserWithMessage("Hello Mattia!");
+    }
+
+    private void givenARequestIsComingInForUser(String username) {
+        when(request.getRequestDispatcher(GREETING_RESULT_VIEW)).thenReturn(requestDispatcher);
+        when(request.getParameter(USERNAME_PARAMETER)).thenReturn(username);
+    }
+
+    private void whenTheRequestIsReceived() throws ServletException, IOException {
+        greeter.doGet(request, response);
+    }
+
+    private void thenTheRequestIsForwardedToTheGreetingsPage() throws ServletException, IOException {
+        verify(requestDispatcher).forward(request, response);
+    }
+
+    private void thenTheGreetinsPageWillGreetTheUserWithMessage(String expectedGreetingMessage) {
+        verify(request).setAttribute(GREETING_MESSAGE_PARAMETER, expectedGreetingMessage);
+    }
+
 }
