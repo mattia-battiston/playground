4984ca919cc6c5e8969a95f9f2e78221e9e13fc7
mattia.battiston <mattia.battiston@gmail.com>
2014-05-14 21:09:47 +0100

    configuring the application with mvc

--- spring-webapp/src/main/java/com/playground/springwebapp/AppConfig.java
+++ spring-webapp/src/main/java/com/playground/springwebapp/AppConfig.java
@@ -1,10 +1,15 @@
 package com.playground.springwebapp;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.playground.springwebapp"})
 @Configuration
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public MyService myService() {
---
+++ spring-webapp/src/main/java/com/playground/springwebapp/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.playground.springwebapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @Autowired
+    private MyService myService;
+
+    @RequestMapping("/helloWorld")
+    public String helloWorld(Model model) {
+        model.addAttribute("message", "Hello World!");
+        System.out.println("in the controller");
+        myService.doStuff();
+        return "result";
+    }
+
+}
