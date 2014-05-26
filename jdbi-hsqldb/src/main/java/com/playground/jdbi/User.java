ef20d26fe7038288ede0ab2d9b9e8961fd139c94
mattia.battiston <mattia.battiston@gmail.com>
2014-05-26 17:25:04 +0100

    trying various strategies for selecting data

---
+++ jdbi-hsqldb/src/main/java/com/playground/jdbi/User.java
@@ -0,0 +1,31 @@
+package com.playground.jdbi;
+
+public class User {
+
+    private String username;
+    private String name;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- jdbi-hsqldb/src/test/java/com/playground/jdbi/BasicUsage.java
+++ jdbi-hsqldb/src/test/java/com/playground/jdbi/BasicUsage.java
@@ -4,7 +4,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
+import org.skife.jdbi.v2.Query;
 import org.skife.jdbi.v2.util.StringMapper;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
@@ -51,4 +55,23 @@ public class BasicUsage {
         database.close();
     }
 
+    @Test
+    public void selectList() throws Exception {
+        database.execute(" create table users (" +
+                "       username varchar(50) primary key, " +
+                "       name varchar(100)" +
+                "   )");
+
+        database.insert("insert into users(username, name) values (?, ?)", "mattia", "Mattia Battiston");
+        database.insert("insert into users(username, name) values (?, ?)", "tony", "Tony Battiston");
+
+        List<Map<String, Object>> select = database.select("select * from users");
+        System.out.println("select = " + select);
+
+        List<Map<String, Object>> createQuery = database.createQuery("select * from users").list();
+        System.out.println("createQuery = " + createQuery);
+
+        List<User> users = database.createQuery("select * from users").map(User.class).list();
+        System.out.println("users = " + users);
+    }
 }
