319bc5682450798d14e025c22795c4a28e393ce8
mattia.battiston <mattia.battiston@gmail.com>
2014-07-27 11:35:54 +0100

    following the hello world example from dropwizard

---
+++ dropwizard/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -0,0 +1,33 @@
+package com.example.helloworld;
+
+import com.example.helloworld.health.TemplateHealthCheck;
+import com.example.helloworld.resources.HelloWorldResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        environment.jersey().register(resource);
+
+        final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+    }
+
+}
---
+++ dropwizard/src/main/java/com/example/helloworld/Main.java
@@ -0,0 +1,9 @@
+package com.example.helloworld;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+}
