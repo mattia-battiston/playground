3b96210d273a30e36fdd6f350c71efdd2921f4b1
mattia.battiston <mattia.battiston@gmail.com>
2014-08-18 22:32:10 +0100

    implemented authentication based on basic auth header; service is annotated with roles to allow

--- dropwizard/src/main/java/com/example/helloworld/authentication/ServerRestrictedToInjector.java
+++ dropwizard/src/main/java/com/example/helloworld/authentication/ServerRestrictedToInjector.java
@@ -5,6 +5,8 @@ import com.sun.jersey.api.core.HttpContext;
 import com.sun.jersey.server.impl.inject.AbstractHttpContextInjectable;
 import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.basic.BasicCredentials;
+import org.eclipse.jetty.util.B64Code;
+import org.eclipse.jetty.util.StringUtil;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.HttpHeaders;
@@ -15,12 +17,12 @@ public class ServerRestrictedToInjector<T> extends AbstractHttpContextInjectable
 
     private final UserRoleAuthenticator userRoleAuthenticator;
     private final String realm;
-    private final UserRole[] value;
+    private final UserRole value;
 
     public ServerRestrictedToInjector(UserRoleAuthenticator userRoleAuthenticator, String realm, UserRole[] value) {
         this.userRoleAuthenticator = userRoleAuthenticator;
         this.realm = realm;
-        this.value = value;
+        this.value = value[0];
     }
 
     @Override
@@ -33,12 +35,27 @@ public class ServerRestrictedToInjector<T> extends AbstractHttpContextInjectable
 
                 System.out.println("Authorization header: " + header);
 
-                // TODO decript basic auth header
-                BasicCredentials credentials = new BasicCredentials("role1", "secret");
+                final int space = header.indexOf(' ');
+                final String decoded = B64Code.decode(header.substring(space + 1), StringUtil.__ISO_8859_1);
+                final int colonIndex = decoded.indexOf(':');
+                final String username = decoded.substring(0, colonIndex);
+                final String password = decoded.substring(colonIndex + 1);
+                BasicCredentials credentials = new BasicCredentials(username, password);
 
                 final Optional<User> result = userRoleAuthenticator.authenticate(credentials);
                 if (result.isPresent()) {
-                    return (T) result.get();
+                    User user = result.get();
+                    if(user.hasRole(value)) {
+                        return (T) result.get();
+                    } else {
+                        // user does not have permissions
+                        throw new WebApplicationException(
+                                Response.status(Response.Status.UNAUTHORIZED)
+                                        .header(HttpHeaders.AUTHORIZATION, realm)
+                                        .entity("Credentials are required to access this resource.")
+                                        .type(MediaType.TEXT_PLAIN_TYPE)
+                                        .build());
+                    }
                 }
             } else {
                 System.out.println("No authorization header!");
--- dropwizard/src/main/java/com/example/helloworld/resources/HelloAuthenticationResource.java
+++ dropwizard/src/main/java/com/example/helloworld/resources/HelloAuthenticationResource.java
@@ -23,4 +23,12 @@ public class HelloAuthenticationResource {
         return "Hello authenticated " + user.getUsername();
     }
 
+    @Path("role2")
+    @GET
+    @Timed
+    @Consumes({MediaType.TEXT_PLAIN, MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.TEXT_HTML, "application/octet-stream"})
+    public String sayHelloRole2(@RestrictedTo({UserRole.ROLE_2}) User user) {
+        return "Hello authenticated " + user.getUsername();
+    }
+
 }
