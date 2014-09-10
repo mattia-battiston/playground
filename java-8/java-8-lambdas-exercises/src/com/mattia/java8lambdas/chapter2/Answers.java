fe73c167c619004d8bd542df2544435586da9461
mattia.battiston <mattia.battiston@gmail.com>
2014-09-10 21:22:39 +0100

    setup project to work on java 8 exercises

---
+++ java-8/java-8-lambdas-exercises/src/com/mattia/java8lambdas/chapter2/Answers.java
@@ -0,0 +1,35 @@
+package com.mattia.java8lambdas.chapter2;
+
+import org.junit.Test;
+
+import javax.swing.text.DateFormatter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+public class Answers {
+
+    @Test
+    public void question1() {
+        //a. T -> Function -> R
+        //b. something that operates on one input, e.g. not
+        //c.
+        // x -> x + 1; yes
+        // (x, y) -> x + 1; no, should take only one input
+        // x -> x ==1; no, should return a Long
+    }
+
+    @Test
+    public void question2() {
+        ThreadLocal<DateFormat> formatterThreadLocal = ThreadLocal.withInitial( () -> new SimpleDateFormat("dd-MMM-yyyy") );
+
+        String formattedDate = formatterThreadLocal.get().format(new Date());
+
+        System.out.println("formattedDate = " + formattedDate);
+    }
+
+    
+
+}
